428d0f16effcd48a5999e5debd7c09e601cfb04d
mythunlearner <learntechpassion@gmail.com>
2025-06-22 09:53:18 +0530

    Implemented Mangoose scheme functions for validatepassword and jwt token generation

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -63,6 +65,21 @@ const userSchema = new mongoose.Schema({
     }
 },
 {timestamps : true});
+
+//Schema Method to generate JWT token
+userSchema.methods.getJWT = async function() {
+  const user = this;
+  const token = await jwt.sign({_id:user._id},"Milan@2020",{expiresIn:"1h"});
+  return token;
+};
+
+//Schema method to validate password
+userSchema.methods.validatePassword = async function(passwordInputByUser) {
+    const user = this;
+    const passwordHash = user.password;
+    const isPasswordValid = await bcrypt.compare(passwordInputByUser,passwordHash);
+    return isPasswordValid;
+}
 
 const user =  mongoose.model("user",userSchema);
 
